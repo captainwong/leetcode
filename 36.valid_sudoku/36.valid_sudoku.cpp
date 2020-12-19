042d7d5111a0e45b2f1f6f8b04f8323765e11ba3
i7-8700 <1281261856@qq.com>
2020-12-20 04:05:28 +0800

    faster

--- 36.valid_sudoku/36.valid_sudoku.cpp
+++ 36.valid_sudoku/36.valid_sudoku.cpp
@@ -1,12 +1,14 @@
 
 #include "../common/leetcode.h"
 
-/*
-执行用时：48 ms, 在所有 C++ 提交中击败了18.07%的用户
-内存消耗：20.7 MB, 在所有 C++ 提交中击败了5.02%的用户
-*/
+
 class Solution {
 public:
+
+    /*
+    执行用时：48 ms, 在所有 C++ 提交中击败了18.07%的用户
+    内存消耗：20.7 MB, 在所有 C++ 提交中击败了5.02%的用户
+    */
     bool isValidSudoku(vector<vector<char>>& board) {
         if (board.size() != 9) { return false; }
         for (const auto& line : board) { if (line.size() != 9) return false; }
@@ -37,7 +39,40 @@ public:
 
         return true;
     }
+
+    /*
+    执行用时：32 ms, 在所有 C++ 提交中击败了60.97%的用户
+    内存消耗：18.1 MB, 在所有 C++ 提交中击败了25.52%的用户
+    */
+    bool isValidSudoku2(vector<vector<char>>& board) {
+        if (board.size() != 9) { return false; }
+        for (const auto& line : board) { if (line.size() != 9) return false; }
+        bool rows[9][9] = { false };
+        bool cols[9][9] = { false };
+        bool blocks[9][9] = { false };
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+
+                int val = board[i][j] - '0' - 1;
+
+                if (rows[i][val]) { return false; }
+                rows[i][val] = true;
+
+                if (cols[j][val]) { return false; }
+                cols[j][val] = true;
+
+                int k = j / 3 + i / 3 * 3;
+                if (blocks[k][val]) { return false; }
+                blocks[k][val] = true;
+            }
+        }
+
+        return true;
+    }
 };
+
+
 
 int main()
 {
@@ -56,9 +91,11 @@ int main()
 	};
 
     assert(Solution().isValidSudoku(input1));
+    assert(Solution().isValidSudoku2(input1));
     
     input1[0][0] = '8';
     assert(!Solution().isValidSudoku(input1));
+    assert(!Solution().isValidSudoku2(input1));
     
 }
 
